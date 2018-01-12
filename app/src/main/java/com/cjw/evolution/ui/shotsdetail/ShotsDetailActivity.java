11f403b65d56bb74443d45d400fc42f1f74f3703
chenjianwei <938548157@qq.com>
2018-01-12 16:47:28 +0800

    library and build tools upgrade

--- app/src/main/java/com/cjw/evolution/ui/shotsdetail/ShotsDetailActivity.java
+++ app/src/main/java/com/cjw/evolution/ui/shotsdetail/ShotsDetailActivity.java
@@ -3,6 +3,7 @@ package com.cjw.evolution.ui.shotsdetail;
 import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
@@ -16,6 +17,8 @@ import android.transition.Transition;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
@@ -77,6 +80,17 @@ public class ShotsDetailActivity extends BaseActivity implements ShotsDetailCont
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Window window = getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+                | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(Color.TRANSPARENT);
+        window.setNavigationBarColor(Color.TRANSPARENT);
+
         setContentView(R.layout.activity_shots_detail);
         getWindow().getSharedElementReturnTransition().addListener(shotReturnHomeListener);
         ButterKnife.bind(this);
